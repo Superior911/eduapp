2f8748031486b1922a94f111510f40e5a416ad18
Superior911 <131838855+Superior911@users.noreply.github.com>
2023-05-15 01:55:57 +0700

    Update home_screen.dart

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,252 +1,198 @@
-import 'package:eduapp/screen/course_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomePage extends StatelessWidget {
-  List catNames = [
-    'Category',
-    'classes',
-    'Free Course',
-    'BlookStore',
-    'Live Course',
-    'LeaderBoard',
-  ];
-
-  List<Color> catColors = [
-    Color(0xFFFFCF2F),
-    Color(0xFF6FE08D),
-    Color(0xFF61BDFD),
-    Color(0xFFFC7F7F),
-    Color(0xFFCB84FB),
-    Color(0xFF78E667),
-  ];
-
-  List<Icon> catIcons = [
-    Icon(Icons.category, color: Colors.white, size: 30),
-    Icon(Icons.video_library, color: Colors.white, size: 30),
-    Icon(Icons.assignment, color: Colors.white, size: 30),
-    Icon(Icons.store, color: Colors.white, size: 30),
-    Icon(Icons.play_circle_fill, color: Colors.white, size: 30),
-    Icon(Icons.emoji_events, color: Colors.white, size: 30),
-  ];
-
-  List imgList = [
-    'Flutter',
-    'React Native',
-    'Python',
-    'C#',
-  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        children: [
-          Container(
-            padding: EdgeInsets.only(top: 15, left: 15, right: 15, bottom: 10),
-            decoration: BoxDecoration(
-              color: Color(0xFF674AEF),
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(20),
-                bottomRight: Radius.circular(20),
-              ),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(120),
+        child: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0.0,
+          toolbarHeight: 70,
+          title: Align(
+            alignment: Alignment.bottomCenter,
+            child: Image.asset(
+              'images/rmutlogo.png',
+              height: 300,
+              width: 300,
             ),
-            child: Column(
-              //crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Icon(
-                      Icons.dashboard,
-                      size: 30,
-                      color: Colors.white,
-                    ),
-                    Icon(
-                      Icons.notifications,
-                      size: 30,
-                      color: Colors.white,
-                    ),
-                  ],
-                ),
-                SizedBox(height: 20),
-                Padding(
-                  padding: EdgeInsets.only(left: 3, bottom: 15),
-                  child: Text(
-                    'Hi progarmmer ',
+          ),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.notifications),
+              onPressed: () {},
+            ),
+          ],
+          flexibleSpace: Container(
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.only(bottomRight: Radius.circular(20)),
+              color: Color(0xFF790E9F),
+            ),
+          ),
+          systemOverlayStyle: SystemUiOverlayStyle.light,
+        ),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: const BoxDecoration(
+                color: Color(0xFF790E9F),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'ยินดีต้อนรับ',
                     style: TextStyle(
-                      fontSize: 25,
-                      fontWeight: FontWeight.w600,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w300,
                       letterSpacing: 1,
                       wordSpacing: 2,
                       color: Colors.white,
                     ),
                   ),
-                ),
-                Container(
-                  margin: EdgeInsets.only(top: 5, bottom: 20),
-                  width: MediaQuery.of(context).size.width,
-                  height: 55,
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                      border: InputBorder.none,
-                      hintText: 'Search here...',
-                      hintStyle: TextStyle(
-                        color: Colors.black.withOpacity(0.5),
-                      ),
-                      prefixIcon: Icon(
-                        Icons.search,
-                        size: 25,
-                      ),
+                  const SizedBox(height: 10),
+                  Text(
+                    'ชื่อผู้ใช้',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.white.withOpacity(0.7),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(top: 20, left: 15, right: 15),
-            child: Column(
-              children: [
-                GridView.builder(
-                  itemCount: catNames.length,
-                  shrinkWrap: true,
-                  physics: NeverScrollableScrollPhysics(),
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 3,
-                    childAspectRatio: 1.1,
-                  ),
-                  itemBuilder: (context, index) {
-                    return Column(
-                      children: [
-                        Container(
-                          height: 60,
-                          width: 60,
-                          decoration: BoxDecoration(
-                            color: catColors[index],
-                            shape: BoxShape.circle,
-                          ),
-                          child: Center(
-                            child: catIcons[index],
-                          ),
-                        ),
-                        SizedBox(height: 10),
-                        Text(
-                          catNames[index],
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w500,
-                            color: Colors.black.withOpacity(0.7),
-                          ),
-                        ),
-                      ],
-                    );
-                  },
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      'Couress',
-                      style: TextStyle(
-                        fontSize: 23,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                    Text(
-                      'See All',
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.w500,
-                        color: Color(0xFF674AEF),
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 10),
-                GridView.builder(
-                  itemCount: imgList.length,
-                  shrinkWrap: true,
-                  physics: NeverScrollableScrollPhysics(),
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    childAspectRatio:
-                        (MediaQuery.of(context).size.height - 50 - 25) /
-                            (4 * 240),
-                    mainAxisSpacing: 10,
-                    crossAxisSpacing: 10,
-                  ),
-                  itemBuilder: (context, index) {
-                    return InkWell(
-                      onTap: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => CouseScreen(imgList[index]),
-                            ));
-                      },
-                      child: Container(
-                        padding:
-                            EdgeInsets.symmetric(vertical: 20, horizontal: 10),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          color: Color(0xFFF5F3FF),
-                        ),
-                        child: Column(
-                          children: [
-                            Padding(
-                              padding: EdgeInsets.all(10),
-                              child: Image.asset(
-                                'images/${imgList[index]}.png',
-                                width: 55,
-                                height: 55,
-                              ),
-                            ),
-                            SizedBox(height: 10),
-                            Text(
-                              imgList[index],
-                              style: TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.w600,
-                                color: Colors.black.withOpacity(0.6),
-                              ),
-                            ),
-                            SizedBox(height: 10),
-                            Text(
-                              '55 Videos',
-                              style: TextStyle(
-                                fontSize: 15,
-                                fontWeight: FontWeight.w500,
-                                color: Colors.black.withOpacity(0.5),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    );
-                  },
-                ),
-              ],
+            ListTile(
+              title: const Text('หน้าแรก'),
+              leading: const Icon(Icons.home),
+              onTap: () {
+                // Handle drawer item tap
+              },
             ),
-          ),
-        ],
+            ListTile(
+              title: const Text('ช่วยเหลือ'),
+              leading: const Icon(Icons.chat),
+              onTap: () {
+                // Handle drawer item tap
+              },
+            ),
+            ListTile(
+              title: const Text('เปลี่ยนภาษา'),
+              leading: const Icon(Icons.language),
+              onTap: () {
+                // Handle drawer item tap
+              },
+            ),
+            ListTile(
+              title: const Text('ข้อมูลเพิ่มเติม'),
+              leading: const Icon(Icons.priority_high),
+              onTap: () {
+                // Handle drawer item tap
+              },
+            ),
+          ],
+        ),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        iconSize: 32,
-        selectedItemColor: Color(0xFF674AEF),
-        selectedFontSize: 18,
-        unselectedItemColor: Colors.grey,
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.assignment), label: 'Courses'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.favorite), label: 'Wishlist'),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Account'),
-        ],
+      body: Container(
+        child: Column(
+          mainAxisAlignment:
+              MainAxisAlignment.start, // Change to MainAxisAlignment.start
+          children: [
+            InkWell(
+              onTap: () {
+                // Add your desired functionality here
+              },
+              child: Container(
+                width:100,
+                height: 50,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.grey[200], // Customize the background color
+                ),
+                child: const Icon(
+                  Icons.assessment, // Replace with your desired icon
+                  color: Colors.black, // Customize the icon color
+                ),
+              ),
+            ),
+            SizedBox(height: 20), // Add spacing between buttons
+            InkWell(
+              onTap: () {
+                // Add your desired functionality here
+              },
+              child: Container(
+                width: 50,
+                height: 50,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.grey[200], // Customize the background color
+                ),
+                child: const Icon(
+                  Icons.person, // Replace with your desired icon
+                  color: Colors.black, // Customize the icon color
+                ),
+              ),
+            ),
+            SizedBox(height: 20), // Add spacing between buttons
+            InkWell(
+              onTap: () {
+                // Add your desired functionality here
+              },
+              child: Container(
+                width: 50,
+                height: 50,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.grey[200], // Customize the background color
+                ),
+                child: const Icon(
+                  Icons.menu_book, // Replace with your desired icon
+                  color: Colors.black, // Customize the icon color
+                ),
+              ),
+            ),
+            SizedBox(height: 20), // Add spacing between buttons
+            InkWell(
+              onTap: () {
+                // Add your desired functionality here
+              },
+              child: Container(
+                width: 50,
+                height: 50,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.grey[200], // Customize the background color
+                ),
+                child: const Icon(
+                  Icons.store, // Replace with your desired icon
+                  color: Colors.black, // Customize the icon color
+                ),
+              ),
+            ),
+            SizedBox(height: 20), // Add spacing between buttons
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Icon(Icons.newspaper, size: 30),
+                SizedBox(
+                  width: 10,
+                ),
+                Text(
+                  'RMUTT NEWS',
+                  style: TextStyle(
+                    fontSize: 23,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
